640db31caa5758eedea07709172749332f1bcf85
mohdrafik <rafikiitbhu@gmail.com>
2025-03-05 14:43:11 +0100

    added new module for listing a specific file classified on the basis of the last part of the name: given as key as argument and path

---
+++ src/modules/listspecificfiles.py
@@ -0,0 +1,29 @@
+import os
+import glob
+
+
+def readlistFiles(filepath,keyword):
+    """ Read the list of specific files based on the key word given by users:
+        Enter the filepath and keyword (e.g. "normalized.npy", ...)
+    """
+    # keyword = "*keyword";
+    retSpecificFiles = []
+    Files = glob.glob(os.path.join(filepath,f"*{keyword}"))
+    for file in Files:
+        Filename = os.path.basename(file)
+    
+        retSpecificFiles.append(Filename)
+
+
+    return retSpecificFiles;
+
+if __name__=="__main__":
+    
+    # Keyword = 'normalized.npy'
+    Keyword = input("enter keyword:")
+    # if not Keyword:
+    # filepath = input("enter Filepath:")    
+    filepath = r'E:\\Projects\\substructure_3d_data\\Substructure_Different_DataTypes\\data\\intermdata1'
+    print("output res: --> \n",readlistFiles(filepath=filepath,keyword=Keyword))
+
+
